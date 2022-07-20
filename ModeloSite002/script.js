d7f8eb42edbb404244201ad0c62986b7eeed2c73
Carlos Antônio <carlosaljuniorcg@hotmail.com>
2022-07-20 14:55:32 -0300

    Update 20072022

---
+++ ModeloSite002/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res') 
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'BebeHomem.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'JovemHomem.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'AdultoHomem.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'IdosoHomem.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'BebeMulher.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'JovemMulher.png')
+            } else if (idade < 60) {
+                //Adulto
+                img.setAttribute('src', 'AdultaMulher.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'IdosaMulher.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+
+    }
+}
---
+++ ex015_Repetições.js
@@ -0,0 +1,31 @@
+for (var c = 1; c <= 6; c++) {
+    console.log('Tudo bem?')
+}
+
+/*
+//Escrever de forma repetida uma determinada mensagem usando do.
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 10)
+*/
+
+/*
+//Escrever uma determinada mensagem de forma repetida no while.
+var c = 1
+while (c <= 6) {
+    console.log('Tudo em ordem?')
+    c++
+}
+*/
+
+/*
+//Escrever uma determinada mensagem de forma repetida.
+console.log('Tudo em ordem?')
+console.log('Tudo em ordem?')
+console.log('Tudo em ordem?')
+console.log('Tudo em ordem?')
+console.log('Tudo em ordem?')
+console.log('Tudo em ordem?')
+*/
